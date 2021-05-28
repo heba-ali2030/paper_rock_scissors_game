4493ed23970e7d52adb7332a1f90d865ecb5c6d8
heba-ali2030 <heba.galaa@gmail.com>
2021-05-28 20:08:57 +0200

    added and upgraded

--- README.md
+++ README.md
@@ -12,11 +12,11 @@
    #### * "scissors" (a fist with the index finger and middle finger extended, forming a V). "Scissors" is identical to the two-fingered V sign (also indicating "victory" or "peace") except that it is pointed horizontally instead of being held upright in the air.
 
 ## Who is the winner?
-   
-   ### A player who decides to play rock will beat another player who has chosen scissors ("rock crushes scissors" or sometimes "blunts scissors"
 
-   ### but rock will lose to one who has played paper ("paper covers rock"); 
+###     Each of the three basic handsigns (from left to right: rock, paper, and scissors) beats one of the other two, and loses to the other.
+   
+   ### 1- A player who decides to play rock will beat another player who has chosen scissors ("rock crushes scissors" or sometimes "blunts scissors" and will lose to one who has played paper ("paper covers rock"); 
     
-   ### a play of paper will lose to a play of scissors ("scissors cuts paper").
+   ### 2- a play of paper will lose to a play of scissors ("scissors cuts paper").
     
-   ### If both players choose the same shape, the game is tied and is usually immediately replayed to break the tie. 
+   ### 3- If both players choose the same shape, the game is tied and is usually immediately replayed to break the tie. 
---
+++ Rock_paper_scissors.py
@@ -0,0 +1,69 @@
+
+import random
+
+# Number of times for playing the game:
+print (f'Hello, You can play 5 times')
+
+run = 0
+while run < 5: 
+    run += 1
+    if run == 5:
+        print('It is the last time to play!')
+    else:
+        print(f'It is the {run} run')
+
+    # ask each player to choose Rock, paper or scissors
+    player1 = input('please type Rock, paper or Scissors: \n ').casefold()
+
+    hand_game = ['Rock', 'Paper' ,'Scissors']
+    player2 = random.choice(hand_game).casefold()
+    
+    # the user and system are differrent:
+    if player1 != player2:
+        print(f'you played {player1} and your compititor played a {player2}')
+
+        # 1- player plays rock
+        if player1 == 'rock':
+            if player2 == 'scissors':
+                print(f'Congratulations, You are the winner ^_^, Rock crushes scissors')
+            
+            # elif player2 == 'paper':
+            else:
+                print(f'ooooops, You are the loser ^_^, Paper covers rock')
+
+        # 2- player plays paper
+        elif player1 == 'paper':
+            if player2 == 'rock':
+               print(f'Congratulations, You are the winner ^_^, Paper covers rock')
+
+            # elif player2 == 'scissors':
+            else:
+               print(f'Ooooops, You are the loser!, scissors cuts paper')
+
+        # 3- player plays scissor
+
+        elif player1 == 'scissors':
+            if player2 == 'rock':
+                print(f'Ooooops, You are the loser !, Rock crushes scissors')
+
+            # elif  player2 == 'paper':
+            else:
+                print(f'Congratulations, You are the winner ^_^, scissors cuts paper')
+        
+    # player and the system are the same
+    else: 
+        print(f'Ooooooh ! You played the same {player1} as system, try again!')
+
+    # to leaave space between runs:
+    print('     ')
+    
+
+# when run is >5
+else: 
+    print('The game is over')
+
+
+
+
+
+
