c6157ea6caf46ad593e35a39f9f63da1673b533d
heba-ali2030 <heba.galaa@gmail.com>
2021-05-31 06:16:00 +0200

    new feature is added to exit game  at any time

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -16,8 +16,6 @@ run = 0
 while True: 
     run += 1
     
-    # tell the user about turns
-
     print(f'It is the {run} run')
 
     # ask each player to choose Rock, paper or scissors
@@ -33,38 +31,51 @@ while True:
         # 1- player plays rock
         if player1 == 'rock':
             if player2 == 'scissors':
-                print(f'Congratulations, You are the winner ^_^, Rock crushes scissors')
+                print(f'Congratulations, You are the winner ^_^, Rock crushes scissors\n')
             
             # elif player2 == 'paper':
             else:
-                print(f'ooooops, You are the loser ^_^, Paper covers rock')
+                print(f'ooooops, You are the loser ^_^, Paper covers rock\n')
 
         # 2- player plays paper
         elif player1 == 'paper':
             if player2 == 'rock':
-               print(f'Congratulations, You are the winner ^_^, Paper covers rock')
+               print(f'Congratulations, You are the winner ^_^, Paper covers rock\n')
 
             # elif player2 == 'scissors':
             else:
-               print(f'Ooooops, You are the loser!, scissors cuts paper')
+               print(f'Ooooops, You are the loser!, scissors cuts paper\n')
 
         # 3- player plays scissor
 
         elif player1 == 'scissors':
             if player2 == 'rock':
-                print(f'Ooooops, You are the loser !, Rock crushes scissors')
+                print(f'Ooooops, You are the loser !, Rock crushes scissors\n')
 
             # elif  player2 == 'paper':
             else:
-                print(f'Congratulations, You are the winner ^_^, scissors cuts paper')
+                print(f'Congratulations, You are the winner ^_^, scissors cuts paper\n')
         
     # player and the system are the same
     else: 
-        print(f'Ooooooh ! You played the same {player1} as system, try again!')
+        print(f'Ooooooh ! You played the same {player1} as system, try again!\n')
+    
+    
+    # ask the user to complete to the next level or to exit and save level.
+    play_again= input(' Do you want to complete the game ? Type No  or N if yow want to exit: ').casefold()
+    if play_again == 'no' or play_again == 'n':
+        print('waiting to see you again !')
+        break
+    else:
+        pass
 
     # to leaave space between runs:
     print('     ')
     
+    
+
+
+
 
 
 
